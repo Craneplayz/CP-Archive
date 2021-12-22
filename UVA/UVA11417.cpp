4bd191498c1f70e9687266152f195370852b53d4
Craneplayz <96546412+Craneplayz@users.noreply.github.com>
2021-12-23 03:10:15 +0800

    Add files via upload

---
+++ UVA/UVA11417.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Declarations
+typedef deque<int> dqi;
+typedef long long ll;
+typedef list<int> lsi;
+typedef map<int,int> mii; 
+typedef map<int,char> mic; 
+typedef map<char,int> mci;
+typedef map<string,int> msi; 
+typedef map<string,pair<int,int>> mspii; 
+typedef map<string,map<int,int>> msmii;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pl;
+typedef pair<string,int> psi;
+typedef queue<int> qi;
+typedef vector<int> veci;
+typedef vector<int>::iterator itveci; 
+typedef vector<vector<int>> veci2;
+typedef vector<long long> vecl;
+typedef vector<vector<long long>> vecl2;
+typedef vector<float> vecf;
+typedef vector<vector<float>> vecf2;
+typedef vector<string> vecs;
+typedef vector<vector<string>> vecs2;
+typedef vector<pair<int,int>> vecpii;
+typedef vector<pair<long long,long long>> vecpll;
+typedef vector<pair<string,int>> vecpsi;
+typedef vector<bool> vecb;
+typedef vector<vector<bool>> vecb2;
+typedef string str;
+
+//Functions
+#define elif else if
+#define len length
+#define sz size
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+
+//Debug
+// #define cin fin
+// #define cout fout
+
+ofstream fout ("test.out");
+ifstream fin ("test.in");
+
+ll Gdp[501];
+
+ll gcd(ll a,ll b){
+   if(b==0) return a;
+   return gcd(b,a%b);
+}
+
+ll G(ll N){
+   if(Gdp[N]!=-1) return Gdp[N];
+   ll k=0;
+   for(ll i=1;i<N;i++){
+      k+=gcd(i,N);
+   }
+   return Gdp[N]=G(N-1)+k;
+}
+
+
+void solve(){
+   memset(Gdp,-1,sizeof(Gdp));
+   Gdp[2]=1;
+   ll n;
+   while(cin>>n){
+      if(!n) break;
+      cout<<G(n)<<'\n';
+   }
+}
+
+int main(){
+   //Fast Input
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+    
+   //Driving Code
+   solve();
+    
+   return 0;
+}
